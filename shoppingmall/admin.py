ef46e7d7e3bec6a5877883707a57494208d4679d
조미정 <ing06164@naver.com>
2022-12-14 21:26:23 +0900

    장고 프로젝트 생성 & models.py 구성

---
+++ shoppingmall/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Toy, Maker, Category, Material
+# Register your models here.
+
+admin.site.register(Toy)
+# name필드 값으로 slug 자동 생성
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}   # 카테고리가 자동으로 slug로 만들어짐?
+admin.site.register(Category, CategoryAdmin)
+class MaterialAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}   # 카테고리가 자동으로 slug로 만들어짐?
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Maker)
---
+++ shoppingmall/models.py
@@ -0,0 +1,51 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.contrib.auth.models import User
+
+# Material - 다대다
+class Material(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+# Maker, Category - 다대일
+class Maker(models.Model):
+    name = models.CharField(max_length=30, unique=True)     # 제조사명
+    address = models.CharField(max_length=50, unique=True)  # 주소
+
+    # 연락처
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
+
+    # 설립년도
+    establishment = models.DateField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)   # 카테고리는 unique해야 함
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    class Meta:   # verbose_name_plural은 예약어
+        verbose_name_plural = 'Categories'   # admin페이지의 Categorys대신 들어감
+
+class Toy(models.Model):
+    title = models.CharField(max_length=30)      # 상품명
+    hook_text = models.CharField(max_length=100, blank=True)   # 미리보기 텍스트
+    content = models.TextField()
+
+    # 이미지 업로드
+    head_image = models.ImageField(upload_to='shoppingmall/images/%Y/%m/%d/', blank=True)  # 이미지 저장 경로
+
+    # 상품 가격
+    price = models.IntegerField()
+
+    # 제조사(다대일)
+    maker = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # 카테고리(다대일)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    # 재질 (다대다)
+    material = models.ManyToManyField(Material, blank=True)
+
+    # 출시년도
+    created_at = models.DateTimeField()
+
