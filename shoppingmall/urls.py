962ed914dbdb5928d8ee33f1c7f9305e6b534197
조미정 <ing06164@naver.com>
2022-12-15 20:00:47 +0900

    toy_detail - 상세 페이지 구현(댓글 제외)

--- shoppingmall/urls.py
+++ shoppingmall/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ToyList.as_view()),
 
+    path('<int:pk>/', views.ToyDetail.as_view()),
     path('category/<str:slug>/', views.category_page),  # IP주소/shopping/category/slug/
     path('material/<str:slug>/', views.material_page),  # IP주소/shopping/material/slug/
     path('maker/<int:pk>/', views.maker_page),
